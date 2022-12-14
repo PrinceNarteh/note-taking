38051084a7f2e83e533b69dc5d9520e8c358bead
Prince Narteh <prinarteh@gmail.com>
2022-12-14 04:45:00 -0800

    add tailwind css framework

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <div>
+    <div className="w-screen h-screen flex justify-center items-center">
       <h1>Note Taking Application</h1>
     </div>
   );
